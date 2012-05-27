4fcd042e379ff201f316f2f1eb21c2369b307f48
John Webster <jlwebster@gmail.com>
2012-05-27 00:05:48 -0600

    test

--- src/CanvasRenderingContext2d.cc
+++ src/CanvasRenderingContext2d.cc
@@ -1683,7 +1683,7 @@ Context2d::SetFont(const Arguments &args) {
 	/* Load our fonts */
 	FT_Face ft_face;
 	printf("first %d", 0);
-	printf("success? %d\n", FT_New_Face(ft_library, "/Users/jwebster/dev/typeshirt-canvas/fonts/tt0001m_.ttf", 0, &ft_face));
+	FT_New_Face(ft_library, "/Users/jwebster/dev/typeshirt-canvas/fonts/tt0001m_.ttf", 0, &ft_face);
 	printf("test? %d", 1);
 	printf("success char size? %d", FT_Set_Char_Size(ft_face, 0, ptSize, device_hdpi, device_vdpi ));
 	printf("hi there %d", 2);
